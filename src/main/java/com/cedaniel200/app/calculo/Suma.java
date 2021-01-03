953e69bfc01286db5cced8428a552e1c92ebe5a3
Dllorca <>
2021-01-03 20:03:41 +0100

    segon commit curso cucumber

--- src/main/java/com/cedaniel200/app/calculo/Suma.java
+++ src/main/java/com/cedaniel200/app/calculo/Suma.java
@@ -3,8 +3,10 @@ package com.cedaniel200.app.calculo;
 
 //comentari tercer commit
 // comentari brank1
+// comentari2 branca1
+
 public class Suma {
-    public int sumar(int primerNumero, int segundoNumero){
+    public int sumar(int primerNumero, int segundoNumero) {
         return primerNumero + segundoNumero;
     }
 }
